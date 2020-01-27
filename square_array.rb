d842f43b7a8cbd66a5ab8bcae0b02cd47dd13090
fadimata tillman <fadi.tillman@gmail.com>
2020-01-27 23:02:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,8 @@ def square_array(array)
   array = [1,2,3]
   array.each{|number| puts "square_array doesn't call collect/map or inject"}
 end
+
+def calls_on_each
+  arry = [1,2,3]
+  array.each {|number| puts **2}
+end 
